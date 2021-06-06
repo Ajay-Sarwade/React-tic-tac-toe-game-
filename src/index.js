97e3f0fb5a3fcc040b270c95a96816502f1e4059
Ajay <ajaysarwade339@gmail.com>
2021-06-06 20:00:03 +0530

    edited

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Square = (props) => {
+  const [value, setValue] = useState(null);
+  return (
+
+    <button className='square' onClick={props.onclicking}   >{props.value}</button>
+  );
+};
+
+
+
+
+const Board = () => {
+
+  const list = [null, null, null, null, null, null, null, null, null];
+  const [squares, setsquare] = useState(list);
+  const [xisnext, isxnext] = useState(true);
+
+  const handleClick = (i) => {
+    const list1 = [...squares];
+    if (list1[i] == null && !winner(squares)) {
+      list1[i] = xisnext ? 'X' : "O";
+      setsquare(list1);
+      isxnext(!xisnext);
+    }
+
+  };
+
+
+ 
+
+  const win = winner(squares);
+  const status = win ? ('WINNER: ' + win) : (xisnext ? 'NEXT PLAYER:X' : 'NEXT PLAYER:O');
+
+  const rendersqare = (i) => {
+    return (
+      <Square value={squares[i]} onclicking={() => handleClick(i)} />
+    );
+  };
+
+  return (
+    <div className='board'>
+      {status}
+      <div className='row-square'>{rendersqare(0)} {rendersqare(1)}{rendersqare(2)} </div>
+      <div className='row-square'>{rendersqare(3)}{rendersqare(4)}{rendersqare(5)}  </div>
+      <div className='row-square'>{rendersqare(6)} {rendersqare(7)}{rendersqare(8)}  </div>
+    </div>
+  );
+};
+
+function winner(list) {
+  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  for (let line of lines) {
+    const [a, b, c] = line;
+    if (list[a] && list[a] == list[b] && list[a] == list[c]) {
+      return list[a];
+    }
+  }
+  return null;
+
+}
+
+
+
+const Game = () => {
+  return (
+    <div className='game'>
+      TIC-TAC-TOE
+      <Board />
+
+    </div>
+  );
+};
+
+ReactDom.render(<Game />, document.getElementById('root'));
